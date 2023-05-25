6c25c4068d90774c07a2c3ee04fd0e644e8e2f95
20q2 <andrewnintendo@msn.com>
2023-05-24 20:13:24 -0400

    asdf

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [  
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+];
+
+// const routes: Routes = [  
+//   {
+//     path: 'home',
+//     loadChildren: () => import('./app.component').then((m) => m.AppComponent),
+//   },
+//   {
+//     path: 'test',
+//     loadChildren: () => import('./components/test-page/test-page.component').then((m) => m.TestPageComponent),
+//   },
+//   {
+//     path: 'error',
+//     loadChildren: () => import('./components/error-page/error-page.component').then((m) => m.ErrorPageComponent),
+//   },
+//   {
+//     path: '**',
+//     redirectTo: 'error',
+//     pathMatch: 'full'
+//   }  
+// ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
